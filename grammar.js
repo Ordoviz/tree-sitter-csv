4f58d3255e0e09506f8673635e234a47bcc0399c
Lennard Hofmann <lennard.hofmann@web.de>
2026-08-17 18:29:35 +0200

    Fix empty and single-letter fields
    This drops support for field separators other than comma for simplicity.

--- grammar.js
+++ grammar.js
@@ -1,74 +1,46 @@
-let rules_for_sep = (sep, sym) => {
-  let r = {};
-  r[`_csv${sym}`] = ($) => repeat1(alias($[`row${sym}`], $.row));
-  r[`row${sym}`] = ($) =>
-    seq(
-      repeat(seq($[`_cycle7${sym}`], sep)),
-      optional($[`_remainder${sym}`]),
-      "\n",
-    );
-
-  r[`_cycle7${sym}`] = ($) =>
-    seq(
-      optional(alias($[`field${sym}`], $.first)),
-      sep,
-      optional(alias($[`field${sym}`], $.second)),
-      sep,
-      optional(alias($[`field${sym}`], $.third)),
-      sep,
-      optional(alias($[`field${sym}`], $.fourth)),
-      sep,
-      optional(alias($[`field${sym}`], $.fifth)),
-      sep,
-      optional(alias($[`field${sym}`], $.sixth)),
-      sep,
-      optional(alias($[`field${sym}`], $.seventh)),
-    );
-  r[`_remainder${sym}`] = ($) =>
-    seq(
-      alias($[`field${sym}`], $.first),
-      ...[$.seventh, $.sixth, $.fifth, $.fourth, $.third, $.second].reduce(
-        (accum, fld) => [
-          optional(seq(sep, optional(alias($[`field${sym}`], fld)), ...accum)),
-        ],
-        [],
-      ),
-    );
-
-  r[`field${sym}`] = ($) =>
-    choice(
-      prec(3, $.boolean),
-      prec(2, $.number),
-      prec(1, $.float),
-      prec(0, alias($[`text${sym}`], $.text)),
-    );
-  r[`text${sym}`] = (_) =>
-    token(
-      choice(
-        new RegExp(`[^-+${sep}\\d\\s"][^${sep}\\n\\r"]+`),
-        seq('"', repeat(choice(/[^"]/, '""')), '"'),
-      ),
-    );
-  return r;
-};
-
 module.exports = grammar({
   name: "csv",
 
   rules: {
-    csv: ($) => choice($._csvc, $._csvs, $._csvp),
+    csv: ($) => repeat1(alias($[`row`], $.row)),
 
-    ...rules_for_sep(",", "c"),
-    ...rules_for_sep(";", "s"),
-    ...rules_for_sep("|", "p"),
+    row: ($) => seq(
+        repeat(seq($[`_cycle7`], ",")),
+        optional($[`_remainder`]),
+        "\n"
+    ),
 
-    number: (_) => choice(/[-+]?\d+/, /0[xX][0-9a-fA-F]+/),
-    float: (_) => choice(/\d*\.\d+/, /[-+]?\d+\.\d*/),
-    boolean: (_) => choice("true", "false"),
+    _cycle7: ($) => seq(
+        alias($[`field`], $.first),
+        ",",
+        alias($[`field`], $.second),
+        ",",
+        alias($[`field`], $.third),
+        ",",
+        alias($[`field`], $.fourth),
+        ",",
+        alias($[`field`], $.fifth),
+        ",",
+        alias($[`field`], $.sixth),
+        ",",
+        alias($[`field`], $.seventh)
+    ),
+
+    _remainder: ($) => seq(
+        alias($[`field`], $.first),
+        ...[$.seventh, $.sixth, $.fifth, $.fourth, $.third, $.second].reduce(
+            (accum, fld) => [
+                optional(seq(",", alias($[`field`], fld), ...accum)),
+            ],
+            []
+        )
+    ),
+
+    field: ($) => token(
+        choice(
+            new RegExp(`[^,\\n\\r"]*`),
+            seq('"', repeat(choice(/[^"]/, '""')), '"')
+        )
+    ),
   },
-
-  conflicts: ($) => [
-    [$.rowc, $.rows, $.rowp],
-    [$.fieldc, $.fields, $.fieldp],
-  ],
-});
+})
--- readme.md
+++ readme.md
@@ -1,9 +1,4 @@
 ## Rainbow CSV Tree Sitter
 
-Designed to parse CSV files for the [Zed Rainbow CSV extension](https://github.com/weartist/zed-rainbow-csv).
-
-### Supported Separators
-
-- Comma (`,`)
-- Semicolon (`;`)
-- Pipe (`|`)
+Designed to colorize the columns in CSV files (comma-separated values).
+Only commas (`,`) are supported as field separators.
